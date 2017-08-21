d0aefb20e5fefc52e767fbd9057cace5576c9ec4
GavinThomas1192 <gthomas1192@gmail.com>
2017-08-21 15:04:49 -0700

    second

--- class-01/__test__/index.test.js
+++ class-01/__test__/index.test.js
@@ -5,7 +5,10 @@ const class01 = require('../class-01.js')
 
 describe('class-01.js', function() {
   //specific to jest === test()
-  test('default properties', () => {
+  describe('default properties', () => {
+  test('should have a default name of xxx', () => {
     expect(class01.name).toEqual('Katherine')
+    expect(true).toBeFalsy();
   })
+)};
 });
